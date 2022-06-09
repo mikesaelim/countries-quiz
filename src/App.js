dcaccbd58eab88e2049000bec36f326206b47b69
Mike Saelim <mike.saelim@gmail.com>
2022-06-09 12:49:35 -0500

    Janky page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { Button, Form } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content mx-auto border">
+        <div className="title m-5 border">
+          This is the title.
+        </div>
+        <div className="game container border">
+          <div className="row border">
+            <div className="col-sm-8 border">
+              <Form className='m-2'>
+                <Form.Group>
+                  <Form.Control />
+                </Form.Group>
+              </Form>
+            </div>
+            <div className="col-sm-2 border">
+              <div className="m-2">
+                0/200
+              </div>
+            </div>
+            <div className="col-sm-2 border">
+              <div className="m-2">
+                <Button>End</Button>
+              </div>
+            </div>
+          </div>
+          <div className="row border">
+            <div className="col-sm-12 border" style={{height: "400px"}}>
+              oink
+            </div>
+          </div>
+        </div>
+        <div className="description m-5 border">
+          This is the description.
+        </div>
+      </div>
     </div>
   );
 }

b8f1edcbdc15c8bc960cd7948a6646422c1db0d0
Mike Saelim <mike.saelim@gmail.com>
2022-11-05 00:39:37 -0500

    Upgrade @testing-libfrary/user-event to v14

--- src/SpoilerTag.test.js
+++ src/SpoilerTag.test.js
@@ -2,13 +2,14 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SpoilerTag from "./SpoilerTag";
 
-test("hides the spoiler text until clicked", () => {
+test("hides the spoiler text until clicked", async () => {
+  const user = userEvent.setup();
   render(<SpoilerTag>meow</SpoilerTag>);
 
   const element = screen.getByText("meow");
   expect(element).toBeInTheDocument();
   expect(element).toHaveClass("spoiler");
 
-  userEvent.click(element);
+  await user.click(element);
   expect(element).toHaveClass("spoiler-revealed");
 });

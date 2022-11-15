5734e05a21890a198b104d0daab7a81d874fb85f
Mike Saelim <mike.saelim@gmail.com>
2022-11-15 13:07:52 -0600

    Leaflet map with Carto light no label tiles

--- src/MapResults.js
+++ src/MapResults.js
@@ -8,10 +8,24 @@ function MapResults(props) {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    const map = L.map(mapRef.current).setView([30, 0], 2);
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
+    const latLngBounds = [[-60, -180], [75, 180]];
+    const map = L.map(mapRef.current, {
+      center: [30, 0],
+      zoom: 2,
+      minZoom: 2,
+      maxZoom: 5,
+      maxBounds: latLngBounds,
+      maxBoundsViscosity: 1.0
+    });
+
+    // https://github.com/CartoDB/basemap-styles
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}" + (L.Browser.retina ? "@2x.png" : ".png"), {
+      subdomains: "abcd",
+      attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>, &copy; <a href=\"https://carto.com/attributions\">CARTO</a>",
+      minZoom: 2,
+      maxZoom: 5,
+      bounds: latLngBounds,
+      noWrap: true
     }).addTo(map);
 
     return () => map.remove();

fd6401561c2f16c2d274d5888c275db42057070d
Mike Saelim <mike.saelim@gmail.com>
2022-11-15 14:01:07 -0600

    Use react-leaflet

--- src/MapResults.js
+++ src/MapResults.js
@@ -1,39 +1,33 @@
 import L from "leaflet";
-import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 import "./MapResults.css";
 import "leaflet/dist/leaflet.css";
 
 function MapResults(props) {
-  const mapRef = useRef(null);
-
-  useEffect(() => {
-    const latLngBounds = [[-60, -180], [75, 180]];
-    const map = L.map(mapRef.current, {
-      center: [30, 0],
-      zoom: 2,
-      minZoom: 2,
-      maxZoom: 5,
-      maxBounds: latLngBounds,
-      maxBoundsViscosity: 1.0
-    });
-
-    // https://github.com/CartoDB/basemap-styles
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}" + (L.Browser.retina ? "@2x.png" : ".png"), {
-      subdomains: "abcd",
-      attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>, &copy; <a href=\"https://carto.com/attributions\">CARTO</a>",
-      minZoom: 2,
-      maxZoom: 5,
-      bounds: latLngBounds,
-      noWrap: true
-    }).addTo(map);
-
-    return () => map.remove();
-  }, []);
-
+  const latLngBounds = [[-60, -180], [75, 180]];
   return (
     <div className="map-results">
-      <div ref={mapRef} className="map" />
+      <MapContainer
+        center={[30, 0]}
+        zoom={2}
+        minZoom={2}
+        maxZoom={5}
+        maxBounds={latLngBounds}
+        maxBoundsViscosity={1.0}
+        className="map"
+      >
+        <TileLayer
+          // https://github.com/CartoDB/basemap-styles
+          url={"https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}" + (L.Browser.retina ? "@2x.png" : ".png")}
+          subdomains="abcd"
+          attribution={"&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>, &copy; <a href=\"https://carto.com/attributions\">CARTO</a>"}
+          minZoom={2}
+          maxZoom={5}
+          bounds={latLngBounds}
+          noWrap={true}
+        />
+      </MapContainer>
     </div>
   );
 }

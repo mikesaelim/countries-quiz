8c68e05db2cec3f54d616a2123fc63ed2bab680f
Mike Saelim <mike.saelim@gmail.com>
2023-01-16 20:14:47 -0600

    Copy default files from the CRA template with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,10 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
     <App />
